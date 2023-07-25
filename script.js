89a59240c265eb4e34563d82716ded2813c8d5c6
Melvin <melvinhkusuma@gmail.com>
2023-07-25 14:59:24 +1000

    rerolls counter decrement

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-var isInitialLoad = true;
+var isTitleScreen = true;
+var rerollsLeft = 3;
 
-const dice = document.getElementById("rolled-dice").children;
-for (let die of dice) {
+const dice = document.getElementById("rolled-dice");
+const rerollsLeftText = document.getElementById("rerolls");
+
+for (let die of dice.children)
     die.addEventListener("click", (e) => e.target.classList.toggle("hold"));
-}
 
 document.getElementById("roll").onclick = rollDice;
 
 function rollDice() {
-    if (isInitialLoad) {
-        document.getElementById("rolled-dice").style.display = "flex";
-        document.getElementById("rerolls").style.display = "initial";
+    if (rerollsLeft == 0)
+        return;
+    if (isTitleScreen) {
+        dice.style.display = "flex";
+        rerollsLeftText.style.display = "initial";
         document.getElementById("title-screen").style.display = "none";
-        isInitialLoad = false;
+        isTitleScreen = false;
     }
 
-    for (let die of dice) {
+    for (let die of dice.children)
         if (!die.classList.contains("hold")) {
             const getRandomNumber = Math.floor(Math.random() * 6) + 1;
             die.style.backgroundImage = `url("dice/${getRandomNumber}.svg")`;
         }
-    }
+    rerollsLeft--;
+    rerollsLeftText.innerText = `Rerolls left: ${rerollsLeft}`;
 }
 
 function getRolledDice() {
     const result = [];
 
-    for (let die of dice) {
+    for (let die of dice.children) {
         const spliced = getComputedStyle(die).backgroundImage.split('/');
         result.push(spliced[spliced.length - 1][0]);
     }
