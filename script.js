a538e751f238c17d01a09615d3daa08e9473ed20
Melvin <melvinhkusuma@gmail.com>
2023-07-25 16:19:47 +1000

    calc basic categories

--- script.js
+++ script.js
@@ -1,44 +1,48 @@
 var isTitleScreen = true;
-var rerollsLeft = 3;
+var rerolls = 3;
+var currentDiceNumbers = [];
 
 const dice = document.getElementById("rolled-dice");
-const rerollsLeftText = document.getElementById("rerolls");
+const rerollsText = document.getElementById("rerolls");
 
 for (let die of dice.children)
     die.addEventListener("click", (e) => e.target.classList.toggle("hold"));
 
 function rollDice() {
-    if (rerollsLeft == 0)
+    if (rerolls == 0)
         return;
+
     if (isTitleScreen) {
         dice.style.display = "flex";
-        rerollsLeftText.style.display = "initial";
+        rerollsText.style.display = "initial";
         document.getElementById("title-screen").style.display = "none";
         isTitleScreen = false;
-    }
+    } else
+        currentDiceNumbers = [];
 
     for (let die of dice.children)
         if (!die.classList.contains("hold")) {
             const getRandomNumber = Math.floor(Math.random() * 6) + 1;
+            currentDiceNumbers.push(getRandomNumber);
             die.style.backgroundImage = `url("dice/${getRandomNumber}.svg")`;
         }
-    rerollsLeft--;
-    rerollsLeftText.innerText = `Rerolls left: ${rerollsLeft}`;
+
+    rerolls--;
+    rerollsText.innerText = `Rerolls left: ${rerolls}`;
 }
 
-function calculateCategories() {
-    return;
+function calculateBasicCategories() {
+    ["aces", "deuces", "threes", "fours", "fives", "sixes"].forEach((name, index) => {
+        var result = 0;
+        currentDiceNumbers.forEach((value) => {
+            if (value == index + 1)
+                result += value;
+        });
+        document.getElementById(name).innerText = result;
+    });
 }
 
-document.getElementById("roll").addEventListener("click", () => { rollDice(); calculateCategories(); });
-
-function getRolledDice() {
-    const result = [];
-
-    for (let die of dice.children) {
-        const spliced = getComputedStyle(die).backgroundImage.split('/');
-        result.push(spliced[spliced.length - 1][0]);
-    }
-
-    return result;
-}
+document.getElementById("roll").addEventListener("click", () => {
+    rollDice();
+    calculateBasicCategories();
+});
